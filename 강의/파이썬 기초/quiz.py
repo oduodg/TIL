72268372615dcd7b50c6ecc877bb0011d437058a
Eun <werewildfire@gmail.com>
2021-08-23 19:29:40 +0900

    파이썬 기초 공부5

--- 강의/파이썬 기초/quiz.py
+++ 강의/파이썬 기초/quiz.py
@@ -23,25 +23,65 @@ for n in range(1,51):
         print(report_file.read())
 """
 
-class AttackUnit: # 공격 유닛
-		def __init__(self, name, hp, damage):
-				self.name = name
-				self.hp = hp
-				self.damage = damage
-		def attack(self, location): # 전달 받은 방향으로 공격
-				print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]" \
-							.format(self.name, location, self.damage)) # 공간이 좁아서 2줄에 걸쳐 출력
-		def damaged(self, damage): # damage 만큼 유닛 피해
-				print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage)) # 데미지 정보 출력
-				self.hp -= damage # 유닛의 체력에서 전달받은 damage 만큼 감소
-				print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp)) # 남은 체력 출력
-				if self.hp <= 0: # 남은 체력이 0 이하이면?
-					print("{0} : 파괴되었습니다.".format(self.name)) # 유닛 파괴 처리
+"""
+# 퀴즈 8
+"""
 
-    # 파이어뱃 : 공격 유닛, 화염방사기.
-firebat1 = AttackUnit("파이어뱃", 50, 16) # 체력 50, 공격력 16
-firebat1.attack("5시") # 5시 방향으로 공격 명령
+""" 
+class House:
+    # 매물 초기화 : 위치, 건물 종류, 매물 종류, 가격, 준공년도
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
 
-# 공격 2번 받는다고 가정
-firebat1.damaged(25) # 남은 체력 25
-firebat1.damaged(25) # 남은 체력 0
+    # 매물 정보 표시
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, self.price, self.completion_year)
+
+apt = House("강남", "아파트", "매매", "10억", "2010년")
+office = House("마포", "오피스텔", "전세", "5억", "2007년")
+villa = House("송파", "빌라", "월세", "500/50", "2000년")
+
+houses = []
+houses.append(apt)
+houses.append(office)
+houses.append(villa)
+
+print(f"총 {len(houses)}대의 매물이 있습니다.")
+for house in houses:
+    house.show_detail() 
+    """
+
+chicken = 10 # 남은 치킨 수
+waiting = 1 # 홀 안에는 현재 만석. 대기번호 1부터 시작
+
+class Sold_out_error(Exception): # 사용자 정의 에러
+    pass
+
+
+while(True):
+    try:
+        print("[남은 치킨 : {0}]".format(chicken))
+        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
+        if order > chicken: # 남은 치킨보다 주문량이 많을 때
+            print("재료가 부족합니다.")
+        elif order <= 0: 
+            raise ValueError
+        else:
+            print("[대기번호 {0}] {1} 마리 주문이 완료되었습니다.".format(waiting, order))
+            waiting += 1 # 대기번호 증가
+            chicken -= order # 주문 수 만큼 남은 치킨 감소
+        
+        if chicken == 0: # 남은 치킨 수가 0 이면
+            raise Sold_out_error # 더 이상 주문 불가하므로 재고 소진 에러 발생
+    except ValueError:
+        print("잘못된 값을 입력하였습니다.")
+    except Sold_out_error:
+        print("재고가 소진되어 더 이상 주문을 받지 않습니다.")
+        break # while 반복문 탈출
+    except Exception as err:
+        print("알 수 없는 에러가 발생하였습니다.")
+        print(err)
