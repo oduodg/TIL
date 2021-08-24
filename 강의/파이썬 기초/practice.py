2d98f8a8929307accb67a4ab2b2ea08b2802abc5
Eun <werewildfire@gmail.com>
2021-08-25 00:52:18 +0900

    파이썬 기초 공부6

--- 강의/파이썬 기초/practice.py
+++ 강의/파이썬 기초/practice.py
@@ -1,3 +1,11 @@
-import travel.thailand
-trip_to = travel.thailand.ThailandPackage()
-trip_to.detail()
+import os
+
+folder = "sample_dir"
+
+if os.path.exists(folder):
+    print("이미 존재하는 폴더입니다.")
+    os.rmdir(folder) # 폴더 삭제
+    print(folder, "폴더를 삭제하였습니다.") # 삭제 문구 출력
+else:
+    os.makedirs(folder)
+    print(folder, "폴더를 생성하였습니다.")
