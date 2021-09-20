8324baa0ca6ee9407baf4ef3fe1bd7af5e1cea70
oduodg <werewildfire@gmail.com>
2021-09-21 00:01:36 +0900

    htdocs first commit

---
+++ htdocs/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<meta http-equiv="Content-Type"
+content="text/html; charset=UTF-8" />
+<title>Test PHP</title>
+</head>
+
+
+
+<body>
+<h1>Hello PHP</h1>
+
+<div>
+<?php echo "PHP으로 표시한 텍스트입니다."; ?>
+</div>
+
+<br><br>
+<?php phpinfo(); ?>
+
+</body>
+</html>
