c826741afcfa31f67dd93053e5f2d78295063cd7
oduodg <werewildfire@gmail.com>
2021-10-28 19:09:47 +0900

    21.10.28 commit

---
+++ htdocs/phpjs/11.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        list = new Array("최진혁", "최유빈", "한이람", "한이은","이고잉");
+         i = 0;
+         while(i < list.length){
+             document.write("<li>" + list[i] + "</li>");
+             i = i + 1;
+         }
+    </script>
+
+    <h1>php</h1>
+    <?php
+      $list = array("최진혁", "최유빈", "한이람", "한이은");
+      $i = 0;
+      while($i < count($list)){
+          echo "<li>".$list[$i]."</li>";
+          $i = $i + 1;
+      }
+    ?>
+</body>
+</html>
---
+++ htdocs/phpjs/12.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        function a(){
+            document.write("Hello Function");
+        }
+        a();
+        a();
+        a();
+        a();
+        a();
+    </script>
+    <h1>php</h1>
+    <?php
+        function a(){
+            echo "Hello PHP Function";
+        }
+        a();
+    ?>
+</body>
+</html>
