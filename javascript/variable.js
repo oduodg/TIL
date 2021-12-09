a48a6b80d534970fe1d97dc2680bf7933039430c
oduodg <werewildfire@gmail.com>
2021-12-09 19:03:01 +0900

    JS: async와 defer 차이점

---
+++ javascript/variable.js
@@ -0,0 +1,14 @@
+// 1. Use strict
+// added in ES5
+// use this for Vanila Javascript.
+'use strict';
+
+// 2. Variable
+// let (added in ES6)
+
+let my_name = 'ellie';
+console.log(my_name);
+
+my_name = 'hello';
+console.log(my_name);
+
