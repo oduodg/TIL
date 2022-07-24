005ec3510efb1061aff905cb7cd3528c949532ba
oduodg <werewildfire@gmail.com>
2022-07-24 14:54:32 +0900

    Add header component

--- JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
+++ JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MyHeader from './MyHeader'; // MyHeader 불러오기
 
 function App() {
   let name = "호두호동";
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
         <h2>안녕 리액트! {name}</h2>
       </header>
---
+++ JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/MyHeader.js
@@ -0,0 +1,5 @@
+const MyHeader = () => {
+    return <header>헤더</header>
+};
+
+export default MyHeader; // MyHeader라는 함수를 내보내기 
