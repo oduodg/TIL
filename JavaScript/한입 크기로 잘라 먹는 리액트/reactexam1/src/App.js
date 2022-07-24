6dfe02fb36210ad048c3fb1f5591a95ada26e66c
oduodg <werewildfire@gmail.com>
2022-07-24 15:16:49 +0900

    JSX에 CSS를 inline 방식으로 적용시키기

--- JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
+++ JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
+import React from "react";
+// import './App.css'; // css 파일 불러와서 스타일링할 수 있다.
 import MyHeader from './MyHeader'; // MyHeader 불러오기
 
 function App() {
   let name = "호두호동";
+  
+  const style = {
+    App: {
+      backgroundColor: "black",
+    },
+    h2: {
+      color: "red",
+    },
+    bold_text: {
+      color: "green",
+    },
+  }
   return (
-    <div className="App">
+    <div style={style.App}>
       <MyHeader />
-      <header className="App-header">
-        <h2>안녕 리액트! {name}</h2>
-      </header>
+        <h2 style={style.h2}>안녕 리액트! {name}</h2>
+        <b style={style.bold_text} id="bold_text">React.js</b>
     </div>
   );
 }
