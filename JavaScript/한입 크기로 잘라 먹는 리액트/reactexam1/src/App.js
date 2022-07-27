cd40d6e5f02cbe2f0bb6fe78cc3141d9d74082c3
oduodg <werewildfire@gmail.com>
2022-07-27 16:11:24 +0900

    React Status: Counter 구현해보기

--- JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
+++ JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/App.js
@@ -1,26 +1,13 @@
 import React from "react";
+import Counter from "./Counter";
 // import './App.css'; // css 파일 불러와서 스타일링할 수 있다.
 import MyHeader from './MyHeader'; // MyHeader 불러오기
 
-function App() {
-  let name = "호두호동";
-  
-  const style = {
-    App: {
-      backgroundColor: "black",
-    },
-    h2: {
-      color: "red",
-    },
-    bold_text: {
-      color: "green",
-    },
-  }
+function App() { 
   return (
-    <div style={style.App}>
-      <MyHeader />
-        <h2 style={style.h2}>안녕 리액트! {name}</h2>
-        <b style={style.bold_text} id="bold_text">React.js</b>
+    <div>
+      <MyHeader/>
+      <Counter/>
     </div>
   );
 }
---
+++ JavaScript/한입 크기로 잘라 먹는 리액트/reactexam1/src/Counter.js
@@ -0,0 +1,45 @@
+/* Counter component */
+
+import React,{ useState } from "react"; 
+// 상태를 사용할 수 있는 useState 메서드를 import 해주어야 한다.
+
+const Counter = () => {
+    // + 버튼과 - 버튼을 만들어준다.
+    // 0에서 출발해서
+    // 1씩 증가하고
+    // 1씩 감소하는
+    // count 상태 
+
+    console.log("counter 호출"); // rerender 확인 -> 계속 새로 화면을 그려준다.
+
+    // 첫번째 Counter
+    const [count, setCount] = useState(0);
+    // 0번째 인덱스 count는 "상태의 값"으로 사용된다.
+    // 즉 jsx에서 return을 해서 화면에 표시할 수 있다.
+    // count의 초기값은 useState의 인자로 전달한다.
+    // useState(0) -> count의 초기값을 0으로 초기화함.
+    // 1번째 인덱스 setCount는 count라는 상태를 변화시키는 "상태 변화 함수"로 사용된다.
+
+    const onIncrease = () => {
+        setCount(count + 1);
+    }
+
+    const onDecrease = () => {
+        setCount(count - 1);
+    }
+
+    /* JSX 문법 주의 */
+    // JSX에서는 Camel Case를 사용한다.
+    // onclick이 아닌 onClick
+    // 'onIncrease()'가 아닌 {onIncrease}
+    // 
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
+    )
+}
+
+export default Counter; // Counter 내보내기
